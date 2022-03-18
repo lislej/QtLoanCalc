071af15ce208e0daefbabf34d870c1007547a276
James Lisle <lislej@gmail.com>
2022-03-18 10:20:30 -0400

    soap attributes refactoring

--- versionhandler.cpp
+++ versionhandler.cpp
@@ -3,19 +3,16 @@
 VersionHandler::VersionHandler(QObject* parent) : QObject{parent}
 {
 
-    qDebug() << QThread::currentThread();
-
 }
 
 bool VersionHandler::PingService()
 {
    VersionSoap11Binding m_service;
 
-   m_service.setEndPoint("https://loancalcservice.herokuapp.com/services/Version");
-   m_service.setSoapVersion(KDSoapClientInterface::SOAP1_1);
+   m_service.setEndPoint(versionURL);
+   m_service.setSoapVersion(soapVersion);
 
    NS__GetVersionResponse  resp = m_service.getVersion(NS__GetVersion());
-
 
    return resp.return_().startsWith("Hi - the Axis2 version is 1.7.9" );
 }
--- versionhandler.h
+++ versionhandler.h
@@ -18,6 +18,8 @@ public:
     VersionSoap11Binding* versionsoap11binding();
 
 private:   
+    const char* versionURL="https://loancalcservice.herokuapp.com/services/Version";
+    const KDSoapClientInterface::SoapVersion soapVersion = KDSoapClientInterface::SOAP1_1;
 
 };
 
